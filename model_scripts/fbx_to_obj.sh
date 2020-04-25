bbbd988a0b80a94f71ef31bf7f05e145d376b33a
squidboy3999 <58859289+squidboy3999@users.noreply.github.com>
2020-04-25 01:51:28 -0600

    Update fbx_to_obj.sh

--- model_scripts/fbx_to_obj.sh
+++ model_scripts/fbx_to_obj.sh
@@ -4,7 +4,7 @@ for dir in $(ls -d */); do
 pushd ${dir}
 for f in $(ls | grep .fbx);do
 file_name=${f%.fbx}
-assimp export <file_name>.fbx <file_name>.obj
+assimp export ${file_name}.fbx ${file_name}.obj
 done
 popd
 done
