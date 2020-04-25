f6c1be73e97f2f86cd9d04ab62a4010fd6b9ab1e
squidboy3999 <58859289+squidboy3999@users.noreply.github.com>
2020-04-25 01:32:35 -0600

    Update fbx_to_obj.sh

--- model_scripts/fbx_to_obj.sh
+++ model_scripts/fbx_to_obj.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 # dnf install assimp -y
+for dir in $(ls -d */); do
+pushd ${dir}
+for f in $(ls | grep .fbx);do
 file_name=${f%.fbx}
 assimp export <file_name>.fbx <file_name>.obj
+done
+popd
+done
