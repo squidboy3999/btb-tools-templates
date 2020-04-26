01ad2eaee444bbbe858ff2cd4ad82ce1314567d3
squidboy3999 <58859289+squidboy3999@users.noreply.github.com>
2020-04-25 21:47:37 -0400

    Create make_normal_maps.sh
    Added description of where normalmap tool comes from.

---
+++ model_scripts/make_normal_maps.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+# Source for creating the normal map binary - https://github.com/planrich/normalmap
+# dnf install waf gcc pkg-config ImageMagick-devel
+# build the binary with 'waf configure build'
+# sudo cp build/normalmap /usr/bin/normalmap
