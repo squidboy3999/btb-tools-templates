7f05047370b04f49eeef7eb34098aa6b87902a3f
Mark Mitchell <squidboy3000@gmail.com>
2020-10-17 21:10:33 -0600

    added normal map generation

--- btb_tools_templates/model_converter.py
+++ btb_tools_templates/model_converter.py
@@ -1,64 +1,101 @@
-import bpy
+#import bpy
 import sys
 import os
 from typing import List
 from PIL import Image
+from pathlib import Path
+from normalmap_maker import NormalMapMaker
 
 #blender -b -P model_converter.py
 
 class ModelConvertor:
-    jpeg_names=['.jpg','jpeg','.JPG','.JPEG']
+    img_names=['.jpg','jpeg','.JPG','.JPEG','tga','dds']
 
-    def __init__(self, parent_dir):
-        self.parent_dir=parent_dir
+    def __init__(self, file_dir):
+        #dir = os.path.dirname(__file__)
+        #self.parent_dir=os.path.join(dir, file_dir)
+        self.parent_dir=file_dir
+        print(self.parent_dir)
 
     def recurse_dir(self):
         results=[]
+        results=self.file_actions(os.path.abspath(self.parent_dir))
         for root, subdirs, _ in os.walk(os.path.abspath(self.parent_dir)):
             for subdir in subdirs:
                 full_path=os.path.join(root,subdir)
-                for root_sub, _, files in os.walk(full_path):
-                    for file in files:
-                        file_path=os.path.join(root_sub,file)
-                        results.append(self.action_selector(file_path))
+                print(full_path)
+                results=results+self.file_actions(full_path)
         return results
 
+    def file_actions(self,path):
+        results=[]
+        for root_sub, _, files in os.walk(path):
+            for file in files:
+                file_path=os.path.join(root_sub,file)
+                print(file_path)
+                results.append(self.action_selector(file_path))
+        return results
+
+
     def action_selector(self,file_path):
-        jpeg_names=['.jpg','jpeg','.JPG','.JPEG']
         if '.fbx' in file_path or '.FBX' in file_path:
             self.fbx_to_obj(file_path)
             return file_path+'-FBX2OBJ'
-        for j_name in self.jpeg_names:
-            if j_name in file_path:
-                self.jpg_to_png(file_path)
-                return file_path+'-JPG2PNG'
+        for i_name in self.img_names:
+            if i_name in file_path:
+                self.img_to_png(file_path)
+        self.make_normal(self.img_to_png_ext(file_path))
+        return file_path+'-IMG2PNG'
     
     def fbx_to_obj_ext(self, fbx_path):
         new_path=fbx_path.replace(".fbx",".obj")
         return new_path.replace(".FBX",".obj")
 
     def fbx_to_obj(self,file_path):
-        bpy.ops.import_scene.fbx(filepath = file_path)
-        bpy.ops.export_scene.obj(filepath = fbx_to_obj_ext(file_path))
+        pass
+        #bpy.ops.import_scene.fbx(filepath = file_path)
+        #bpy.ops.export_scene.obj(filepath = fbx_to_obj_ext(file_path))
 
-    def jpg_to_png_ext(self, jpg_path):
-        new_path=jpg_path
-        for j_name in self.jpeg_names:
-            new_path=new_path.replace(j_name,".png")
+    def img_to_png_ext(self, img_path):
+        new_path=img_path
+        for i_name in self.img_names:
+            new_path=new_path.replace(i_name,".png")
         return new_path
 
-    def jpg_to_png(self,file_path):
+    def img_to_png(self,file_path):
         im1 = Image.open(file_path,mode='r')
-        im1.save(self.jpg_to_png_ext(file_path),mode='r')
+        new_path=self.img_to_png_ext(file_path)
+        max_size=512
+        #im1=Image.open(file_path,mode='r')
+        width,height=im1.size
+        if width > max_size or height >max_size:
+            denom=int(height/max_size)
+            if width>height:
+                denom=int(width/max_size)
+            if denom>1:
+                print('{0} is the denom'.format(denom))
+                new_size=(int(width/denom),int(height/denom))
+                print('new_size is {0}'.format(new_size))
+                im1=im1.resize(new_size)
+        im1.save(new_path,mode='r')
+        os.remove(file_path)
+
+    def make_normal(self,file_path):
+        #if normal file doesn't exist
+        if not os.path.isfile(file_path.replace("Diffuse.png","Normal.png")):
+            nm=NormalMapMaker()
+            nm.make_normal(file_path)
+        # create normal maker and convert file
+
 
 USAGE = f"Usage: python {sys.argv[0]} [--help] | parent_dir ]"
 
 def run_convert(args: List[str]):
     try:
-        parent_dir = args[0]
+        work_dir = args[0]
     except TypeError:
         raise SystemExit(USAGE)
-    model_converter=ModelConvertor(parent_dir)
+    model_converter=ModelConvertor(work_dir)
     model_converter.recurse_dir()
 
 def main():
---
+++ btb_tools_templates/normalmap_maker.py
@@ -0,0 +1,77 @@
+import numpy
+import scipy.ndimage as ndi
+import imageio
+import argparse
+import matplotlib.pyplot as plt
+
+
+class NormalMapMaker:
+    intensity=1
+    sig=0
+
+    def sgauss(self,im):
+        if self.sig==0:
+            return im
+        im_sm = im.astype(float)
+        kx= numpy.arrange(-3*sig,3*sig+1).astype(float)
+        kx= numpy.exp((-(kx**2))/(2*(sig**2)))
+        im_sm=ndi.convolve(im_sm,kx[numpy.newaxis])
+        im_sm=ndi.convolve(im_sm,kx[numpy.newaxis].T)
+        return im_sm
+
+    def grad(self,im_sm):
+        gradx = im_sm.astype(float)
+        grady = im_sm.astype(float)
+        k= numpy.arange(-1,2).astype(float)
+        k= -k/2
+        gradx = ndi.convolve(gradx, k[numpy.newaxis])
+        grady = ndi.convolve(grady, k[numpy.newaxis].T)
+        return gradx,grady
+
+    def sbl(self,im_sm):
+        gradx = im_sm.astype(float)
+        grady = im_sm.astype(float)
+        k = numpy.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+        gradx = ndi.convolve(gradx, k)
+        grady = ndi.convolve(grady, k.T)
+        return gradx,grady
+
+    def comp_nm(self,gradx, grady):
+        width = gradx.shape[1]
+        height = gradx.shape[0]
+        max_x = numpy.max(gradx)
+        max_y = numpy.max(grady)
+        max_value = max_x
+        if max_y > max_x:
+            max_value = max_y
+        normal_map = numpy.zeros((height, width, 3), dtype=numpy.float32)
+        #self.intensity = 1 / self.intensity
+        strength = max_value / (max_value * self.intensity)
+        normal_map[..., 0] = gradx / max_value
+        normal_map[..., 1] = grady / max_value
+        normal_map[..., 2] = 1 / strength
+        norm = numpy.sqrt(numpy.power(normal_map[..., 0], 2) + numpy.power(normal_map[..., 1], 2) + numpy.power(normal_map[..., 2], 2))
+        normal_map[..., 0] /= norm
+        normal_map[..., 1] /= norm
+        normal_map[..., 2] /= norm
+        normal_map *= 0.5
+        normal_map += 0.5
+        return normal_map
+
+    def diffuse_to_normal(self, diffuse_path):
+        new_path=diffuse_path.replace("Diffuse.png","Normal.png")
+        return new_path
+
+    def make_normal(self,diffuse_path):
+        img= plt.imread(diffuse_path)
+        if img.ndim == 3:
+            img_grey = numpy.zeros((img.shape[0],img.shape[1])).astype(float)
+            img_grey = (img[...,0] * 0.3 + img[...,1] * 0.6 + img[...,2] * 0.1)
+            img = img_grey
+        img_smooth = self.sgauss(img)
+        sblx, sbly = self.sbl(img_smooth)
+        normal_map = self.comp_nm(sblx, sbly)
+        imageio.imwrite(self.diffuse_to_normal(diffuse_path), normal_map)
+
+#if __name__ == "__main__":
+#    main()
